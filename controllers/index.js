25d29cf92eb7db7e4950684119eb2f43467c18d7
Chad Smith <cds021@yahoo.com>
2023-10-09 09:21:35 -0400

    updating models, seeds, untils and controllers files

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
+
+// Middleware
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+// Exports
+module.exports = router;
